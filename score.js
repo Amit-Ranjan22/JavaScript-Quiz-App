2146103b46a86b3856b3cf8539fef3bf3ec637ad
Amitabh <amitabh.march22@gmail.com>
2021-09-22 11:56:57 -0400

    acess all the requires element and store it in variable

---
+++ score.js
@@ -0,0 +1,4 @@
+let storedScores = JSON.parse(localStorage.getItem("userData"));
+let highScoresArea = document.querySelector("#highScoresList");
+var backBtn = document.querySelector("#backButton");
+var clearBtn = document.querySelector("#clearScores");
