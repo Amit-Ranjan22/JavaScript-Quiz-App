8534fb727661d8b54117bf7ac47378ff4a9955cb
Amitabh <amitabh.march22@gmail.com>
2021-09-22 11:58:26 -0400

    write function to display the score

--- score.js
+++ score.js
@@ -2,3 +2,19 @@ let storedScores = JSON.parse(localStorage.getItem("userData"));
 let highScoresArea = document.querySelector("#highScoresList");
 var backBtn = document.querySelector("#backButton");
 var clearBtn = document.querySelector("#clearScores");
+
+function displayScores() {
+    if(storedScores !== null) {
+        let scoreList = document.createElement("ol");
+        scoreList.className = "scoreListClass";
+        for (let i = 0; i < storedScores.length; i++){
+            let initials = storedScores[i].initial;
+            let scores = storedScores[i].userScore;
+            let scoreEntry = document.createElement("li");
+            scoreEntry.innerHTML = initials + " - " + scores;
+            scoreList.appendChild(scoreEntry);
+      };
+      highScoresArea.appendChild(scoreList);
+    };
+};
+
